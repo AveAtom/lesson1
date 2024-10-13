95ac189f7121b9f957611d17d4168e204fdf07d5
AveAtom <atomos1@mail.ru>
2024-10-14 00:23:51 +0300

    Задача "Неизменяемые и изменяемые объекты. Кортежи и списки"

--- module_1_7.py
+++ module_1_7.py
@@ -3,12 +3,12 @@ print("=== Неизменяемые и изменяемые объекты. Ко
 
 immutable_var = 1, 2, True,"String"# Создайте переменную immutable_var и присвойте ей кортеж из
                                                         # нескольких элементов разных типов данных
-print("Переменная immutable_var - {}".format(immutable_var)) # Выполните операции вывода кортежа immutable_var на экран.
+print("Immutable tuple - {}".format(immutable_var)) # Выполните операции вывода кортежа immutable_var на экран.
 # Попытайтесь изменить элементы кортежа immutable_var. Объясните, почему нельзя изменить значения элементов кортежа.
 # immutable_var[1]=1
 print ("Значение 1 нельзя изменить так как кортеж является не изменяемым объектом (если в нем не находятся изменяемые объекты)")
 
-mutable_list = [x for x in range(3)],True  # Создайте переменную mutable_list и присвойте ей список из нескольких элементов"
-print("Переменная mutable_list - {}".format(mutable_list))
-mutable_list[0][1]=5 # Измените элементы списка mutable_list
-print("Переменная mutable_list после изменения - {}".format(mutable_list)) # Выведите на экран измененный список mutable_list
+mutable_list = [[x for x in range(3)],True]  # Создайте переменную mutable_list и присвойте ей список из нескольких элементов"
+print("\nMutable list - {}".format(mutable_list))
+mutable_list.append("Modified") # Измените элементы списка mutable_list
+print("Mutable list после изменения - {}".format(mutable_list)) # Выведите на экран измененный список mutable_list
